ebd3fa179f0fb9e611c9631b8ea943fada319a57
jolacaleb <jolacaleb@gmail.com>
2020-06-29 19:35:35 +0900

    ImageDiff 기능 프로토타입 개발 완료 (version 1)

---
+++ src/main/java/dev/sunghyun/prototypediffdata/controller/ImageDiffController.java
@@ -0,0 +1,36 @@
+package dev.sunghyun.prototypediffdata.controller;
+
+import com.github.romankh3.image.comparison.ImageComparisonUtil;
+import dev.sunghyun.prototypediffdata.resource.ImageDiff;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+@Controller
+public class ImageDiffController {
+    @PostMapping(path = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    public String getImageDiff(@RequestPart MultipartFile image1, @RequestPart MultipartFile image2, Model model) throws IOException {
+        ImageDiff imageDiff = new ImageDiff(image1, image2);
+
+        double similarity = imageDiff.getSimilarity();
+        BufferedImage result = imageDiff.getDiff();
+
+        // HTML 렌더링하기 위해 BufferedImage을 Base64-encoded string으로 변환하는 작업
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ImageIO.write(result, "png", output);
+
+        model.addAttribute("similarity", similarity);
+        model.addAttribute("result", Base64.getEncoder().encodeToString(output.toByteArray()));
+
+        return "imageDiff";
+    }
+}
---
+++ src/main/java/dev/sunghyun/prototypediffdata/resource/ImageDiff.java
@@ -0,0 +1,43 @@
+package dev.sunghyun.prototypediffdata.resource;
+
+import com.github.romankh3.image.comparison.ImageComparison;
+import com.github.romankh3.image.comparison.ImageComparisonUtil;
+import com.github.romankh3.image.comparison.model.ImageComparisonResult;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@Data
+public class ImageDiff {
+    private final BufferedImage expected;
+    private final BufferedImage actual;
+
+    public ImageDiff(BufferedImage expected, BufferedImage actual) {
+        this.expected = expected;
+        this.actual = actual;
+    }
+
+    public ImageDiff(MultipartFile expected, MultipartFile actual) throws IOException {
+        this.expected = ImageIO.read(new ByteArrayInputStream(expected.getBytes()));
+        this.actual = ImageIO.read(new ByteArrayInputStream(actual.getBytes()));
+    }
+
+    public double getSimilarity() {
+        float differencePercent = ImageComparisonUtil.getDifferencePercent(this.expected, this.actual);
+
+        return 100.0 - differencePercent;
+    }
+
+    public BufferedImage getDiff() {
+        ImageComparison imageComparison = new ImageComparison(expected, actual);
+
+        // TODO: 세세하게 설정할 수 있는 항목: https://romankh3.github.io/image-comparison/#usage
+        ImageComparisonResult imageComparisonResult = imageComparison.compareImages();
+
+        return imageComparisonResult.getResult();
+    }
+}
