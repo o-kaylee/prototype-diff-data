990cb16791454ead034a2d31ffeac97de4841d57
jolacaleb <jolacaleb@gmail.com>
2020-06-29 14:56:08 +0900

    initial commit

---
+++ src/main/java/dev/sunghyun/prototypediffdata/PrototypeDiffDataApplication.java
@@ -0,0 +1,13 @@
+package dev.sunghyun.prototypediffdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeDiffDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeDiffDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sunghyun/prototypediffdata/PrototypeDiffDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sunghyun.prototypediffdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeDiffDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
