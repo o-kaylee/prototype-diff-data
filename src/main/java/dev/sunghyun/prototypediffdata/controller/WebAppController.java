6e17c53c3b24355baed8c58f88b08b0df3209989
jolacaleb <jolacaleb@gmail.com>
2020-06-30 13:45:23 +0900

    index.html에 text diff 부분 default value 추가

--- src/main/java/dev/sunghyun/prototypediffdata/controller/WebAppController.java
+++ src/main/java/dev/sunghyun/prototypediffdata/controller/WebAppController.java
@@ -1,23 +1,18 @@
 package dev.sunghyun.prototypediffdata.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebAppController {
-    private final String appMode;
-
-    @Autowired
-    public WebAppController(Environment environment) {
-        this.appMode = environment.getProperty("app-mode");
-    }
-
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("mode", this.appMode);
+        String defaultOldText = "I am the very model of a modern Major-General, I've information vegetable, animal, and mineral, I know the kings of England, and I quote the fights historical, From Marathon to Waterloo, in order categorical.";
+        String defaultNewText = "I am the very model of a cartoon individual, My animation's comical, unusual, and whimsical, I'm quite adept at funny gags, comedic theory I have read, From wicked puns and stupid jokes to anvils that drop on your head.";
+
+        model.addAttribute("defaultOldText", defaultOldText);
+        model.addAttribute("defaultNewText", defaultNewText);
 
         return "index";
     }
