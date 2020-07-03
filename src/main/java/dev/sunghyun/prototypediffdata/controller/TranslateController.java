a334599b2b3aa784825f7caf7e7cafe9accde1f1
jolacaleb <jolacaleb@gmail.com>
2020-07-03 17:28:30 +0900

    Translator에서 text만 추출하여 번역, 미리 보기 기능 구현
    - 고도화 필요 (디자인, UX 등 ...)

--- src/main/java/dev/sunghyun/prototypediffdata/controller/TranslateController.java
+++ src/main/java/dev/sunghyun/prototypediffdata/controller/TranslateController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 
 @Controller
@@ -33,7 +34,9 @@ public class TranslateController{
 
         Translation translation = new Translation(doc);
 
+        HashMap<String, String> untranslatedText = translation.extractTexts();
         model.addAttribute("previousContent", translation.getRootElement().outerHtml());
+        model.addAttribute("untranslatedText", untranslatedText);
 
         return "translation";
     }
--- src/main/java/dev/sunghyun/prototypediffdata/resource/Translation.java
+++ src/main/java/dev/sunghyun/prototypediffdata/resource/Translation.java
@@ -1,42 +1,50 @@
 package dev.sunghyun.prototypediffdata.resource;
 
 import lombok.Data;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 public class Translation {
     private final Document doc;
     private final Element rootElement;
-    private final Elements childElements;
+    private final Elements sectionElements;
 
     private Map<String, String[]> translatingItems;
 
     public Translation(Document doc) throws IOException {
         this.doc = doc;
         this.rootElement = this.doc.getElementById("aplus");  // todo: find the case for element which id is not `aplus`
-        this.childElements = this.rootElement.select("div.celwidget.aplus-module");
+        this.sectionElements = this.rootElement.select("div.celwidget.aplus-module");
     }
 
-//    private String[] extractTexts() {
-//        Elements textElements = new Elements();
-//
-//        // Consider adding `span` on what condition
-//        for (Element el: this.childElements) {
-//            el.select("h1, h2, h3, h4, h5, h6, p").attr("class", "harpa");
-//
-//
-//            textElements.addAll(el.select("h1, h2, h3, h4, h5, h6, p"));
-//        }
-//
-//        // TODO: 여기서 DB insert, 리턴은 추출한 string만?
-//
-//        ;
-//    }
+    public HashMap<String, String> extractTexts() {
+        HashMap<String, String> textIdMap = new HashMap<>();
+
+        // Consider adding `span` on what condition
+        Elements textElements = this.sectionElements.select("h1, h2, h3, h4, h5, h6, p, span");
+
+        for (int i = 0; i < textElements.size(); i++) {
+            if (!textElements.get(i).text().isEmpty()) {
+                // Declare and define id
+                String elementId = "untranslatedText-" + i;
+
+                // Assign id to elements
+                textElements.get(i).attr("id", elementId);
+
+                textIdMap.put(elementId, textElements.get(i).text());
+            }
+        }
+
+        // TODO: 여기서 DB insert, 리턴은 추출한 string만?
+
+        return textIdMap;
+    }
 }
